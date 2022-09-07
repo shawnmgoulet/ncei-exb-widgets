21a2e2e261c3a8679b329ad17d3207268112ecca
jccartwright <john.c.cartwright@comcast.net>
2022-09-07 15:17:18 -0600

    correct comment

--- widgets/h3-layer/src/runtime/widget.tsx
+++ widgets/h3-layer/src/runtime/widget.tsx
@@ -50,7 +50,7 @@ export default function H3Layer (props: AllWidgetProps<IMConfig>) {
   })
   // console.log('h3-layer: extent', widgetState?.extent)
 
-  // TODO why is queryParams available here but not on line 58
+  // TODO why is queryParams available here but not on line 57
   console.log('h3-layer: queryParams', widgetState?.queryParams)
 
   function displayHexbinSummary (hitTestResult) {
