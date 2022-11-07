2c0b46e20ab139b4836ec5828d5090741a54b190
jccartwright <john.c.cartwright@comcast.net>
2022-11-07 14:38:02 -0700

    experiments w/ using stationary property and debouncing isUpdating changes

--- widgets/map-update-status/src/runtime/widget.tsx
+++ widgets/map-update-status/src/runtime/widget.tsx
@@ -1,79 +1,92 @@
 /** @jsx jsx */
-import { AllWidgetProps, jsx, IMState } from "jimu-core";
-import { useState, useEffect } from 'react';
-import { JimuMapView, JimuMapViewComponent } from "jimu-arcgis";
-import { defaultMessages as jimuUIMessages } from 'jimu-ui';
-import { IMConfig } from '../config';
+import { AllWidgetProps, jsx, IMState } from 'jimu-core'
+import { useState, useEffect, useMemo } from 'react'
+import { JimuMapView, JimuMapViewComponent } from 'jimu-arcgis'
+import reactiveUtils from 'esri/core/reactiveUtils'
+import { defaultMessages as jimuUIMessages } from 'jimu-ui'
+import { IMConfig } from '../config'
 
 interface ExtraProps {
   sqlString: any
 }
 
+// taken from Josh Comeau (https://www.joshwcomeau.com/snippets/javascript/debounce/)
+const debounce = (callback, wait: number) => {
+  let timeoutId = null
+  return (...args) => {
+    window.clearTimeout(timeoutId)
+    timeoutId = window.setTimeout(() => {
+      callback.apply(null, args)
+    }, wait)
+  }
+}
+
 export default function Widget (props: AllWidgetProps<IMConfig> & ExtraProps) {
-  const [view, setView] = useState(null)
-  // useRef instead?
-  const [isStationary, setIsStationary] = useState(true)
+  const [view, setView] = useState<JimuMapView>(null)
   const [isUpdating, setIsUpdating] = useState(false)
-  let stationaryWatch
-  let updatingWatch
 
-  // fires only once, when widget initially opened
+  // TODO debounce not working because need to ensure a "false" value always applied at the end
+  // of an update cycle
+  const handleMapUpdating = useMemo(
+    () => debounce((val) => {
+      setIsUpdating(val)
+    }, 1000),
+    []
+  )
+
   useEffect(() => {
+    if (!view) { return }
+    const mapView = view.view
 
-    // one-time cleanup function
-    return function cleanup() {
-      // remove at time componment is destroyed 
-      if (stationaryWatch) {
-        stationaryWatch.remove()
-        stationaryWatch = null
-      }
-      if (updatingWatch) {
-        updatingWatch.remove()
-        updatingWatch = null
-      }
-    }
-  }, [])
-  
-  // only called when widget first loaded
-  const activeViewChangeHandler = (jmv: JimuMapView) => {
-    if (! jmv) {
-      console.warn('no MapView')
-      return
-    }
+    const extentWatchHandle = reactiveUtils.watch(
+      () => mapView.stationary,
+      (value) => {
+        console.log(`MapView is stationary: ${value}`)
+        // setIsUpdating(!value)
+      })
 
-    setView(jmv.view)
-
-    if (!updatingWatch) {
-      // TODO not sure why this changes so often
-      updatingWatch = jmv.view.watch('updating', (newStatus, oldStatus) => {
-        // console.log(`updating changed from ${oldStatus} to ${newStatus}...`)
+    const updatingWatchHandle = mapView.watch(
+      'updating',
+      (newStatus) => {
+        // handleMapUpdating(newStatus)
         setIsUpdating(newStatus)
       })
-    
-    }
 
-    if (!stationaryWatch) {
-      stationaryWatch = jmv.view.watch('stationary', stationary => {
-        setIsStationary(stationary)
-      });
+    return function cleanup () {
+      // remove at time componment is destroyed
+      if (extentWatchHandle) {
+        extentWatchHandle.remove()
+      }
+      if (updatingWatchHandle) {
+        updatingWatchHandle.remove()
+      }
     }
-  };
+  }, [view])
 
+  // only called when widget first loaded
+  const activeViewChangeHandler = (jmv: JimuMapView) => {
+    if (!jmv) { throw new Error('no MapView') }
+    setView(jmv)
+  }
 
   return (
     <div className="widget-use-map-view" style={{ width: '100%', height: '100%', overflow: 'hidden' }}>
-      <JimuMapViewComponent 
-        useMapWidgetId={props.useMapWidgetIds?.[0]} 
-        onActiveViewChange={activeViewChangeHandler}></JimuMapViewComponent>
-      
-      <div style={{overflowY: 'auto', height: '100%', paddingLeft: '5px'}}>
-        {(!view)?
-           <div>
-            <span>MapView must be defined</span>
-          </div>: ''
-        }        
-        {(isUpdating)? <span style={{fontSize:"large", color:"red"}}>map is updating...</span> : ''}
+      <JimuMapViewComponent
+        useMapWidgetId={props.useMapWidgetIds?.[0]}
+        onActiveViewChange={activeViewChangeHandler}>
+      </JimuMapViewComponent>
+
+      <div style={{ overflowY: 'auto', height: '100%', paddingLeft: '5px' }}>
+        {(!view)
+          ? <div>
+              <span>MapView must be configured</span>
+            </div>
+          : ''
+        }
+        {(isUpdating)
+          ? <span style={{ fontSize: 'medium', color: 'red' }}>map is updating...</span>
+          : ''}
       </div>
     </div>
-  );
+  )
 }
